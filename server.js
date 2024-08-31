42f3ce95257e691048f0ab89b3ea8e6c4a91ea16
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2024-08-31 23:41:42 +0530

    basics

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB Connected !');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
